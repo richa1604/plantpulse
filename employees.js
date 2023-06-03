2701231296d793c3b654fbb01b4230acf8d039dc
Chaitanya Raju <94229891+Reapermaniac@users.noreply.github.com>
2023-06-04 00:16:05 +0530

    Delete employees.js

--- employees.js
+++
@@ -1,9 +0,0 @@
-const express =require('express')
-const router =express.Router()
-const Employeecontoller = require('../contollers/Employeecontoller')
-router.get('/',Employeecontoller.index)
-router.post('/show',Employeecontoller.show)
-router.post('/store',Employeecontoller.store)
-router.post('/update',Employeecontoller.update)
-router.post('/destroy',Employeecontoller.destroy)
-module.exports=router
